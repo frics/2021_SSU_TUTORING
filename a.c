f7ce36aef1a1512b59e9390a980667b4edf6c278
minjoo <meansyou@soongsil.ac.kr>
2021-03-31 01:10:18 -0700

    테스트 파일 수정

--- a.c
+++ a.c
@@ -1,13 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-	printf("kmj\n");
-}#include<stdio.h>
-
-int main(){
-	printf("kmj\n");
-}#include<stdio.h>
-
-int main(){
+int main()
+{
 	printf("kmj\n");
 }
